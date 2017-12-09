b964e6041a0439642f1db2296324ad541d4521e4
Hichame Yessou <hichame.yessou@gmail.com>
2017-12-09 03:50:27 +0100

    First app draft, implemented FB login

---
+++ APP/Travlendar/Travlendar/Dependencies/IParse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Travlendar.Dependencies
+{
+    interface IParse
+    {
+        Task<bool> LoginWithFacebook (string userId, string accessToken, DateTime tokenExpiration);
+    }
+}
---
+++ APP/Travlendar/Travlendar/Renderers/FacebookButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Travlendar.Renderers
+{
+    public class FacebookEventArgs : EventArgs
+    {
+        public string UserId { get; set; }
+        public string AccessToken { get; set; }
+        public DateTime TokenExpiration { get; set; }
+    }
+
+    public class FacebookButton : Button
+    {
+        public Action<object, FacebookEventArgs> OnLogin;
+        public void Login (object sender, FacebookEventArgs args)
+        {
+            if ( OnLogin != null )
+                OnLogin (sender, args);
+        }
+    }
+}
