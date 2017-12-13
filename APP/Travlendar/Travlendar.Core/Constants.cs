d52fa05e47f468149d47f6de330f6e746e83f28e
Hichame Yessou <hichame.yessou@gmail.com>
2017-12-13 01:48:59 +0100

    AWS Cognito Federal Entities Authentication

--- APP/Travlendar/Travlendar.Core/Constants.cs
+++ APP/Travlendar/Travlendar.Core/Constants.cs
@@ -3,9 +3,12 @@
     public class Constants
     {
         //AWS Federal Identities Identity Pool ID
-        public static string AWS_IDENTITY_POOL_ID = "eu-west-1:45c8b421-8dca-403d-9a82-3aea7c9a7879";
+        public const string AWS_IDENTITY_POOL_ID = "eu-west-1:45c8b421-8dca-403d-9a82-3aea7c9a7879";
 
         //Facebook Login App ID
-        public static string FB_APP_ID = "1779284975475344";
+        public const string FB_APP_ID = "1779284975475344";
+
+        //Facebook provide name
+        public const string FB_PROVIDER = "graph.facebook.com";
     }
 }
