30340fe563c53d9af1550f9570afc76c3fc5f158
Hichame Yessou <hichame.yessou@gmail.com>
2017-12-13 01:51:05 +0100

    Minor changes

---
+++ APP/Travlendar/Travlendar.Core/AppCore/Pages/CalendarPage.cs
@@ -0,0 +1,18 @@
+﻿
+using Xamarin.Forms;
+
+namespace Travlendar.Core.AppCore.Pages
+{
+    public class CalendarPage : ContentPage
+    {
+        public CalendarPage ()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "CalendarPage!" }
+                }
+            };
+        }
+    }
+}
