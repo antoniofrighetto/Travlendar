4fc6f34261600d13833c69e660734d057369f747
Hichame Yessou <hichame.yessou@gmail.com>
2017-12-20 23:35:24 +0100

    Implemented Geocoder

--- APP/Travlendar/Travlendar.Core/AppCore/Pages/MapPage.cs
+++ APP/Travlendar/Travlendar.Core/AppCore/Pages/MapPage.cs
@@ -1,24 +1,52 @@
-﻿using Xamarin.Forms;
+﻿using Travlendar.Core.AppCore.ViewModels;
+using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
 namespace Travlendar.Core.AppCore.Pages
 {
     public class MapPage : ContentPage
     {
-        public MapPage ()
+        MapViewModel _viewModel;
+
+        StackLayout stack;
+        Map map;
+        Entry position;
+
+        public MapPage (MapViewModel vm)
         {
-            var map = new Map (
-                MapSpan.FromCenterAndRadius (
-                        new Position (37, -122), Distance.FromMiles (0.3)))
+            _viewModel = vm;
+
+            _viewModel.PropertyChanged += _viewModel_PropertyChanged;
+
+            stack = new StackLayout { Spacing = 0 };
+
+            position = new Entry
+            {
+                Placeholder = "Position",
+                PlaceholderColor = Color.LightGray,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            position.Completed += Position_Completed;
+            stack.Children.Add (position);
+            Content = stack;
+        }
+
+        private void Position_Completed (object sender, System.EventArgs e)
+        {
+            _viewModel.Position = _viewModel.GetPositionFromString (((Entry) sender).Text).Result;
+        }
+
+        private void _viewModel_PropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            map = new Map (
+                            MapSpan.FromCenterAndRadius (_viewModel.Position, Distance.FromMiles (0.3)))
             {
                 IsShowingUser = true,
                 HeightRequest = 100,
                 WidthRequest = 960,
                 VerticalOptions = LayoutOptions.FillAndExpand
             };
-            var stack = new StackLayout { Spacing = 0 };
             stack.Children.Add (map);
-            Content = stack;
         }
     }
 }
---
+++ APP/Travlendar/Travlendar.Core/AppCore/ViewModels/MapViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Travlendar.Core.AppCore.Model;
+using Travlendar.Framework.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace Travlendar.Core.AppCore.ViewModels
+{
+    public class MapViewModel : AViewModel<MapModel>
+    {
+        public override event PropertyChangedEventHandler PropertyChanged;
+
+        Geocoder geoCoder;
+
+        private Position _position;
+        public Position Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                if ( _position != value )
+                {
+                    _position = value;
+                    RaisePropertyChanged ();
+                }
+            }
+        }
+
+        public MapViewModel (INavigation navigation)
+        {
+            this.Navigation = navigation;
+            //TODO for now is a test
+            geoCoder = new Geocoder ();
+        }
+
+        public async Task<Position> GetPositionFromString (string textPosition)
+        {
+            var pos = await GetPositionsForAddressSyncAsync (textPosition);
+            Position = pos [0];
+            return Position;
+        }
+
+        public async System.Threading.Tasks.Task<List<Position>> GetPositionsForAddressSyncAsync (string textPosition)
+        {
+            List<Position> positions = new List<Position> ();
+
+            var approximateLocations = await geoCoder.GetPositionsForAddressAsync (textPosition);
+            foreach ( var position in approximateLocations )
+            {
+                positions.Add (position);
+            }
+
+            return positions;
+        }
+
+        private void RaisePropertyChanged ([CallerMemberName] string property = null)
+        {
+            var propChanged = PropertyChanged;
+            if ( propChanged != null )
+            {
+                propChanged (this, new PropertyChangedEventArgs (property));
+            }
+        }
+    }
+}
