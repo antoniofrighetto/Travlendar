34dd2db5100735fafb60d396622c5a6b7c78f9f3
Unknown <leogivoli@gmail.com>
2017-12-23 13:58:23 +0100

    Added settings page

---
+++ ITD/Travlendar/Travlendar.Core/AppCore/Model/SettingsModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Travlendar.Core.AppCore.Model
+{
+    [JsonObject(MemberSerialization.OptOut)]
+   
+    public class Settings
+    {
+       
+        public int userId { get; set; }
+
+       
+        public int age { get; set; }
+        
+       
+        public bool car { get; set; }
+
+       
+        public bool bike { get; set; }
+
+      
+        public bool publicTransport { get; set; }
+
+       
+        public bool sharedBike { get; set; }
+
+        
+        public bool sharedCar { get; set; }
+
+        
+        public bool minimizeCarbonFootPrint { get; set; }
+
+        
+        public bool lunchBreak { get; set; }
+
+        
+        public TimeSpan timeBreak { get; set; }
+               
+
+    }
+}
