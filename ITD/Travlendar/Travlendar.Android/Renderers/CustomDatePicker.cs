d201f036a0a7535863fe4e30db282ee7f5933df2
Leonardo Givoli <leogivoli@gmail.com>
2017-12-23 14:29:37 +0100

    Added custom renderer android

---
+++ ITD/Travlendar/Travlendar.Android/Renderers/CustomDatePicker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android;
+using Xamarin.Forms;
+using Travlendar.Renderers;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(CustomDatePicker))]
+namespace Travlendar.Renderers
+{
+    public class CustomDatePicker : Xamarin.Forms.Platform.Android.DatePickerRenderer
+    {
+        protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            Control.SetPadding(0, 0, 0, 0);
+
+        }
+    }
+}
