e13fabecbd709c2a4be0cb66f5082f2f9bd2b24d
Leonardo Givoli <leogivoli@gmail.com>
2017-12-23 14:29:37 +0100

    Added custom renderer android

---
+++ ITD/Travlendar/Travlendar.Android/Renderers/CustomEntryCell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Travlendar.Droid;
+using Android.Content;
+using Android.Graphics;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryCell))]
+
+namespace Travlendar.Renderers
+{
+    public class CustomEntryCell : Xamarin.Forms.Platform.Android.EntryRenderer
+
+    {
+        protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            Control.SetPadding(0, 0, 0, 0);
+        }
+
+
+
+    }
+}
---
+++ ITD/Travlendar/Travlendar.Android/Renderers/CustomTimePicker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android;
+using Xamarin.Forms;
+using Travlendar.Renderers;
+
+[assembly: ExportRenderer(typeof(TimePicker), typeof(CustomTimePicker))]
+namespace Travlendar.Renderers
+{
+    public class CustomTimePicker : Xamarin.Forms.Platform.Android.TimePickerRenderer
+    {
+        protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<TimePicker> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            Control.SetPadding(0, 0, 0, 0);
+        }
+
+    }
+}
