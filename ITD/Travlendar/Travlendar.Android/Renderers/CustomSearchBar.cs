fce23fab671d1423b3155eff349609ec21ab69bb
Hichame Yessou <hichame.yessou@gmail.com>
2017-12-29 00:46:29 +0100

    Fix for searchbar not showing on android

---
+++ ITD/Travlendar/Travlendar.Android/Renderers/CustomSearchBar.cs
@@ -0,0 +1,27 @@
+﻿using Android.OS;
+using Travlendar.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer (typeof (SearchBar), typeof (CustomSearchBarRenderer))]
+namespace Travlendar.Droid.Renderers
+{
+    public class CustomSearchBarRenderer : SearchBarRenderer
+    {
+        public CustomSearchBarRenderer ()
+        {
+        }
+
+        protected override void OnElementChanged (ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged (e);
+
+            if ( e.OldElement != null || Element == null )
+                return;
+
+            if ( Build.VERSION.SdkInt >= BuildVersionCodes.N )
+                Element.HeightRequest = 40;
+            Element.BackgroundColor = Color.FromRgb (202, 201, 207);
+        }
+    }
+}
