1af94b21a236bebc48ce4e8dff5315eed5a3975a
Unknown <leogivoli@gmail.com>
2017-12-30 17:35:58 +0100

    Adding picker costum render

---
+++ ITD/Travlendar/Travlendar.Android/Renderers/CustomPicker.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Views;
+using Travlendar.Droid.Renderers;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CustomPicker))]
+namespace Travlendar.Droid.Renderers
+{
+    public class CustomPicker : Xamarin.Forms.Platform.Android.PickerRenderer
+    {
+        public CustomPicker(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+
+            }
+        }
+
+    }
+}
